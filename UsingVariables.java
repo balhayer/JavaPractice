e4cef986f4aa9a3cee94bff7d7815539efb92862
RandyKoiSA <97randy.le@gmail.com>
2018-07-12 00:21:58 -0700

    Added UsingVariables.java
    Use of simple variables

---
+++ UsingVariables.java
@@ -0,0 +1,15 @@
+class UsingVariable{
+	public static void main(String[] args){
+		int roomNumber;
+		double doubleValue;
+		String stringValue;
+		
+		roomNumber = 113;
+		doubleValue = 2.71828;
+		stringValue = "Computer Science";
+		
+		System.out.println("This is room #" + roomNumber);
+		System.out.println("e is close to " + doubleValue);
+		System.out.println("I am learning a bit about " + stringValue);
+	}
+}
