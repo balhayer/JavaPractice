4f17021e7bb4af8b6b7a6b3a5a258ad7b6c14faa
RandyKoiSA <97randy.le@gmail.com>
2018-07-22 00:49:15 -0700

    adding SmileFace project
    Incomplete

---
+++ SmileFace/src/SmileFace.java
@@ -0,0 +1,27 @@
+import java.awt.*;
+import javax.swing.*;
+
+
+public class SmileFace extends Canvas{
+
+    public void paint(Graphics g){
+        Color myFace = new Color(111, 104, 47);
+        Color myEyes = new Color(0,0,100);
+        g.setColor(myFace);
+
+        g.fillOval(200,20,300,300);
+        g.setColor(myEyes);
+
+
+
+    }
+
+    public static void main(String[] args){
+        JFrame win = new JFrame("SmileFace");
+        win.setSize(800,600);
+        win.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        SmileFace canvas = new SmileFace();
+        win.add(canvas);
+        win.setVisible(true);
+    }
+}
