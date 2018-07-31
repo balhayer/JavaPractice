974a911d3be4aee2ea4cea09220cc3b49127dccc
RandyKoiSA <97randy.le@gmail.com>
2018-07-31 01:08:29 -0700

    adding BasicArrays0 project

---
+++ BasicArrays0/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args){
+        int[] sample = new int[10];
+        for(int i = 0; i < sample.length; i++){
+            sample[i] = -117;
+        }
+
+        for(int i = 0; i < sample.length; i++){
+            System.out.print(sample[i]);
+        }
+    }
+}
